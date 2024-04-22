145023c6ae13ffee20af65712ca8a300a85f2d12
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2024-04-22 23:53:31 +0200

    Added ReusableView

---
+++ AlulaMVP/Utils/ReusableView/FloatingTextField/FloatingTextFieldModel.swift
@@ -0,0 +1,14 @@
+//
+//  FloatingTextFieldModel.swift
+//  AlulaMVP
+//
+//  Created by mohamed sliem on 22/04/2024.
+//
+
+import UIKit
+
+struct FloatingTextFieldModel {
+    var icon: UIImage?
+    var title: String?
+    var placeholder: String?
+}
