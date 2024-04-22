08cda2948b3dbff5b967f66c95167e861a2cfae6
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2024-04-22 16:00:36 +0200

    Initial Commit

---
+++ AlulaMVP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlulaMVP
+//
+//  Created by mohamed sliem on 22/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
