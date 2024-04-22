259c3cde3774f493a126ad7828336e0a2d26086b
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2024-04-22 23:38:29 +0200

    Build Splash Screen

--- AlulaMVP/Core/SceneDelegate.swift
+++ AlulaMVP/Core/SceneDelegate.swift
@@ -15,6 +15,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
+        
+        let navigationCotroller = UINavigationController()
+        let rootCoordinator = RootCoordinator(navigationController: navigationCotroller)
+        rootCoordinator.start(animated: true)
+        window?.rootViewController = navigationCotroller
+        
         window?.makeKeyAndVisible()
     }
 
